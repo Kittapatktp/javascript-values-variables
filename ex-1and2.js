aca4bc168067ec7690695548c2edaa5e68a5ad12
Your NamePEem <you@example.comkittapat.ktp@gmail.com>
2026-03-02 20:47:41 +0700

    homework

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,13 @@
 //Exercise #1: Variable Creation
-//Start coding here
+let name = "Karter"
+let lastname = "Anderson"
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
+
+const restaurantName = "Chef's Table by Chef James"
+const totalPrice = 15000
+const activeRestaurant = 1500 
 
 //Exercise #2: Variable Reassignment
-//Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641”//Start coding here"
+totalPrice = 5000
+activeRestaurant = 2500
--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,7 @@
 // Exercise #3: Celsius to Fahrenheit
 let celsius = 30;
-
-// Start coding here;
+let a = (celsius * 1.8) + 32 // Start coding here;
 
 let fahrenheit;
+fahrenheit = a
+console.log(fahrenheit)
